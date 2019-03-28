336908e85cdd70202ad50354ccdcf7d11d6f4ac1
marcowcm <marcowcm@gmail.com>
2019-03-28 14:37:35 +0800

    PresistancePlayerName
    New option for going to next round without clearing players name

--- static/js/Sherlock.js
+++ static/js/Sherlock.js
@@ -1,5 +1,17 @@
-
+var p1 , p2 ,p3 ,p4
 
 $(document).ready(function(){
-    $('#modal1').modal();
+    $('.modal').modal();
   });
+
+function NextRound(){
+	p1 = $('input#player1name').val();
+	p2 = $('input#player2name').val();
+	p3 = $('input#player3name').val();
+	p4 = $('input#player4name').val();
+	$('#playsheet').trigger('reset'); 
+	$('input#player1name').val(p1);
+	$('input#player2name').val(p2);
+	$('input#player3name').val(p3);
+	$('input#player4name').val(p4);
+}
