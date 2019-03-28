a615abfade8de1e1f1c5e608b74fb32d2224d272
marcowcm <marcowcm@gmail.com>
2019-03-28 17:51:21 +0800

    highlightFeature
    highlight feature added for tags
    improved visibility of the form

--- static/js/Sherlock.js
+++ static/js/Sherlock.js
@@ -1,5 +1,6 @@
 var p1 , p2 ,p3 ,p4
 
+
 $(document).ready(function(){
     $('.modal').modal();
   });
@@ -14,4 +15,23 @@ function NextRound(){
 	$('input#player2name').val(p2);
 	$('input#player3name').val(p3);
 	$('input#player4name').val(p4);
+}
+
+function toggle(keyword){
+	if ($('.'+keyword).css('background-color') =='rgba(80, 130, 80, 0.5)'){
+		$('.'+keyword).parent().css('background-color','transparent')
+		$('.'+keyword).css('background-color','transparent');
+	}else{
+		$('.'+keyword).css('background-color','rgba(80, 130, 80, 0.5)');
+	}
+	$('img.'+keyword).parent().each(function(){
+		if($(this).children().filter(function(){return $(this).css('background-color') == 'rgba(80, 130, 80, 0.5)'}).length != 0){
+			$(this).parent().css('background-color','rgba(80, 130, 80, 0.5)');
+			$(this).parent().children('.s2').children(':checkbox').attr('checked', true);
+		}else{
+			$(this).parent().css('background-color','transparent');
+			$(this).parent().children('.s2').children(':checkbox').attr('checked', false);
+		}
+	});
+
 }
